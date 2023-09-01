f7f095d2619d272eca7432fc2f9fb050475b21a4
Daiki Wakabayashi <apollo.daiki@gmail.com>
2023-09-01 13:22:58 +0900

    update replace.py

--- replace.py
+++ replace.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv('log/log.csv')
 
-print(df.replace({' event name': {0: 'allocproc', 1: 'wakeup', 2: 'give up CPU', 3: 'fork', 4: 'tick', 5: 'exit', 6: 'wait', 7: 'sleep'}, ' prev pstate': {0: 'UNUSED', 1: 'EMBRYO', 2: 'SLEEPING', 3: 'RUNNABLE', 4: 'RUNNING', 5: 'ZOMBIE'}, ' next pstate': {0: 'UNUSED', 1: 'EMBRYO', 2: 'SLEEPING', 3: 'RUNNABLE', 4: 'RUNNING', 5: 'ZOMBIE'}}).to_string(index=False))
+print(df.replace({' event name': {0: 'allocproc', 1: 'wakeup', 2: 'giveupCPU', 3: 'fork', 4: 'tick', 5: 'exit', 6: 'wait', 7: 'sleep'}, ' prev pstate': {0: 'UNUSED', 1: 'EMBRYO', 2: 'SLEEPING', 3: 'RUNNABLE', 4: 'RUNNING', 5: 'ZOMBIE'}, ' next pstate': {0: 'UNUSED', 1: 'EMBRYO', 2: 'SLEEPING', 3: 'RUNNABLE', 4: 'RUNNING', 5: 'ZOMBIE'}}).to_string(index=False))
