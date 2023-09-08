df65f398ff2329dd0d8b7d3a22bab67996b323b1
Daiki Wakabayashi <apollo.daiki@gmail.com>
2023-09-08 13:47:40 +0900

    change updating methods and add managing brew

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,54 +1,51 @@
+import shutil
 import numpy as np
 import pandas as pd
+from datetime import datetime
 import matplotlib.pyplot as plt
-from matplotlib.collections import LineCollection
 import matplotlib.ticker as ticker
+from matplotlib.collections import LineCollection
 
-# generate formatted csv file
-df = pd.read_csv('log/log.csv')
-
-# use decimal relative clock
-df['clock'] = df['clock'].apply(lambda x: int(x, 16))
-df['clock'] = df['clock'] - df['clock'][0]
-
-# filter unnecessary info
-# extract RUNNING info
-df.query('pstate_prev == 4 or pstate_next == 4') \
-    .loc[:,['clock', 'cpu', 'pid']] \
-    .to_csv('log/log_formatted.csv', index=False, header=False)
-
-# draw graph
-data_set = np.loadtxt(
-    fname="log/log_formatted.csv",
-    dtype="int",
-    delimiter=",",
-)
-
-fig, ax = plt.subplots()
-# colors = np.random.random_sample((100, 3))
+NPROC = 64
+idx = -1
+labels = [] # for legend
+start_point = [] # start points of line
+sp_idx = [0] * NPROC # index of start point
+cnt_samecpu = [0] * NPROC # number of points in same CPU
+logfile_path = 'log/log.csv'
 colors = ['#ffffff', '#ffffff', '#ffffff', '#ff0000', \
           '#008000', '#0000ff', '#ffd700', '#ff69b4', \
           '#800080', '#808080', '#90ee90', '#8b4513', \
           '#e9967a', '#008080', '#ff00ff', '#556b2f', ]
-labels = [] # for legend
-start_point = [] # start points of line
-sp_idx = [0] * 64 # index of start point
-cnt_samecpu = [0] * 64 # number of points in same CPU
-idx = -1
 
-# seek end point of line
-for ep in data_set: # ep : end_point
+fig, ax = plt.subplots()
+dt = datetime.now()
+exe_time = dt.strftime("%Y_%m%d_%H%M")
+
+# import log
+df = pd.read_csv(logfile_path, header=0)
+shutil.copyfile(logfile_path, "log/" + exe_time + ".csv")
+
+# use decimal and elapsed clock
+df['clock'] = df['clock'].apply(lambda x: int(x, 16))
+df['clock'] -= df['clock'][0]
+
+# extract RUNNING infomation
+df = df.query('pstate_prev == 4 or pstate_next == 4') \
+    .loc[:,['clock', 'cpu', 'pid']] \
+    .astype('int')
+
+# draw line segment
+for ep in df.itertuples(name=None, index=False):
     pid = ep[2]
-    flag = True
+    found_samepid = False
     for sp in start_point:
-        # same pid
         if (sp[2] == pid):
-            flag = False
+            found_samepid = True
 
             # same cpu
             if (sp[1] == ep[1]):
                 cnt_samecpu[pid] += 1
-                # draw line
                 if (cnt_samecpu[pid] % 2 == 0):
                     lc = LineCollection([[[sp[0], sp[1]], [ep[0], ep[1]]]], \
                                         colors=colors[pid], linewidth=10)
@@ -59,27 +56,28 @@ for ep in data_set: # ep : end_point
 
             start_point[sp_idx[pid]] = ep # update start_point
 
-    # same pid is not found in 'start_point'
-    if (flag):
+    if (not found_samepid):
         start_point.append(ep)
         idx += 1
         sp_idx[pid] = idx
 
-    # draw points (invisible but necessary to draw lines)
+    # draw invisible end point
     if (pid in labels):
         ax.scatter(ep[0], ep[1], c=colors[pid], s=100, alpha=0.0)
     else:
         ax.scatter(ep[0], ep[1], c=colors[pid], label=pid, s=100, alpha=0.0)
         labels.append(pid)
 
-# set alpha = 1.0 in legend
+# let legend visible
 leg = ax.legend(title="pid", title_fontsize=11)
 for leha in leg.legend_handles:
     leha.set_alpha(1.0)
 
-plt.gca().get_yaxis().set_major_locator(ticker.MaxNLocator(integer=True)) # use integer in y axis
+# use integer in y axis
+plt.gca().get_yaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+
 plt.xlabel("elapsed clock", fontsize=11)
 plt.ylabel("CPU number", fontsize=11)
-# plt.legend(title="pid", fontsize=11)
 plt.grid()
+plt.savefig("fig/" + exe_time + ".png")
 plt.show()
--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-sample_list = [['要素1', '要素2'], ['要素3', '要素4'], ['要素5', '要素6'], ['要素7', '要素8']]
-print(sample_list)
-
-print('・remove()で2次元リストの要素を取り除く')
-sample_list[0].remove('要素1')
-sample_list[3].remove('要素8')
-sample_list[3].remove('要素7')
-sample_list.remove('要素1')
-print(sample_list)
